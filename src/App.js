b2b381f861d7fbca2cbfd395782515a19f99a670
alligatormonday <joey@joeyjepson.com>
2021-02-25 23:21:31 -0700

    feat-navigation bar started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Motion from './Motion.js';
+import Rentals from './Rentals.js';
+import About from './About.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+
+
+                <Route exact path="/" component={Motion} />
+                <Route path="/rentals" component={Rentals} />
+                <Route path="/about" component={About} />
+
+                <div className="navigation">
+
+                    <div className="navigation-sub">
+
+                        <Link to="/" className="item">Projects</Link>
+                        <Link to="/rentals" className="item">Rentals</Link>
+                        <Link to="/about" className="item">About</Link>
+
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
